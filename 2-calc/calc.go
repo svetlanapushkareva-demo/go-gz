fc83c90ad2403ee9ed9495d1e5dab87da327b356
Svetlana Pushkareva <pushkarevasvetlana2003@gmail.com>
2026-01-29 22:22:59 +0300

    commit Ivana

--- 2-calc/calc.go
+++ 2-calc/calc.go
@@ -30,14 +30,22 @@ func main() {
 		fmt.Println("Неизвестная операция. Используйте: SUM, AVG или MED")
 	}
 }
+
 func scanUserInput() string {
 	var input string
 	for {
 		fmt.Println("Введите операцию (AVG, SUM, MED):")
 		fmt.Scan(&input)
+
+		if input != "AVG" && input != "SUM" && input != "MED" {
+			fmt.Println("Вы ввели неверное значение")
+			continue
+		}
+
 		return input
 	}
 }
+
 func getNumbers() []float64 {
 	var input string
 	fmt.Scan(&input)
@@ -51,38 +59,36 @@ func getNumbers() []float64 {
 			numbers = append(numbers, num)
 		}
 	}
+
 	return numbers
 }
+
 func sum(numbers []float64) float64 {
 	sum := 0.0
 	for _, num := range numbers {
 		sum += num
 	}
-	fmt.Printf("SUM: %.2f\n", sum)
 	return sum
 }
+
 func avg(numbers []float64) float64 {
 	average := sum(numbers) / float64(len(numbers))
-	fmt.Printf("Average: %.2f\n", average)
 	return average
 }
+
 func med(numbers []float64) float64 {
 	sorted := make([]float64, len(numbers))
 	copy(sorted, numbers)
 	sort.Float64s(sorted)
 
-	fmt.Println("Отсортированные числа:", sorted)
-
 	var median float64
 	if len(sorted)%2 == 1 {
 		median = sorted[len(sorted)/2]
-
 	} else {
 		mid1 := sorted[len(sorted)/2-1]
 		mid2 := sorted[len(sorted)/2]
 		median = (mid1 + mid2) / 2
 	}
 
-	fmt.Printf("Median: %.2f\n", median)
 	return median
 }
