f00f691456f0bf787bee90c1134796b60d4fb937
Svetlana Pushkareva <pushkarevasvetlana2003@gmail.com>
2026-02-08 19:09:27 +0300

    3-struct commit

---
+++ 3-struct/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+func newBin(private bool, name string) *Bin {
+	createdAt := time.Now()
+	id := time.Now().String()
+	return &Bin{
+		id:        id,
+		private:   private,
+		createdAt: createdAt,
+		name:      name,
+	}
+}
+
+type BinList []*Bin
+
+func NewBinList() BinList {
+	bl := make(BinList, 0)
+	return bl
+}
+func (bl *BinList) Add(private bool, name string) {
+
+	*bl = append(*bl, newBin(private, name))
+}
+
+func main() {
+	binList := NewBinList()
+	binList.Add(false, "bob")
+
+	fmt.Printf("%+v", binList)
+}
