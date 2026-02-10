24b1de27870ca05e0b9dc649e14befc56f8cfdf5
Svetlana Pushkareva <pushkarevasvetlana2003@gmail.com>
2026-02-10 20:33:32 +0300

    bins commit

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,41 +1,12 @@
 package main
 
 import (
+	"demo/bins/bins"
 	"fmt"
-	"time"
 )
 
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-
-func newBin(private bool, name string) *Bin {
-	createdAt := time.Now()
-	id := time.Now().String()
-	return &Bin{
-		id:        id,
-		private:   private,
-		createdAt: createdAt,
-		name:      name,
-	}
-}
-
-type BinList []*Bin
-
-func NewBinList() BinList {
-	bl := make(BinList, 0)
-	return bl
-}
-func (bl *BinList) Add(private bool, name string) {
-
-	*bl = append(*bl, newBin(private, name))
-}
-
 func main() {
-	binList := NewBinList()
+	binList := bins.NewBinList()
 	binList.Add(false, "bob")
 
 	fmt.Printf("%+v", binList)
---
+++ file/file.go
@@ -0,0 +1,3 @@
+package file
+
+func init() {}
